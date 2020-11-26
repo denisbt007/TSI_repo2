c478d70e509325c74b484201337bd5e326ea92d7
denisbt007 <74795861+denisbt007@users.noreply.github.com>
2020-11-26 18:01:56 -0500

    Update Número primo.py

--- Número primo.py
+++ Número primo.py
@@ -1,1 +1,20 @@
-#42523424
+import math
+
+def DefNúmeroPrimo(num):
+    primo = True
+    print("El número a evaluar es",num)
+    print("")
+
+    for d in range(2, num):
+        if(num%d == 0):
+            print(d,"es divisor de",num)
+            primo = False
+        else:
+            print("No es divisor, siguiente.")
+        print("********")
+        print("")
+
+    if(primo==True):
+        print(num,"es primo.")
+    else:
+        print(num,"no es primo.")
