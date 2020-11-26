fde8fa6589dbad172e6c74ad395667add2db0588
LuteCG <74795646+LuteCG@users.noreply.github.com>
2020-11-26 18:10:51 -0500

    Create Capicua.py

---
+++ Capicua.py
@@ -0,0 +1,21 @@
+num = input("Ingrese el número que quiere averiguar si es capicúa: ")
+capi = True
+izq = []
+der = []
+for i in str(num):
+    der.append(i)
+izq.extend(der)
+der.reverse()
+print(izq)
+print(der)
+
+pos = 0
+for i in izq:
+    if i != der[pos]:
+        capi = False
+    pos += 1
+    
+if capi == True:
+    print(num,"es capicúa.")
+else:
+    print(num,"no es capicúa.")
